73d708ab61d049b0ab60d1f5f00c464fa55fc020
Betheony <bethany@code2college.org>
2023-09-09 23:47:20 -0500

    My stuffs

---
+++ javascript.js
@@ -0,0 +1,13 @@
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+      const container = document.getElementsByClassName('sidebar-container')[0];
+    
+      container.querySelectorAll('#sidebar-open-button, #sidebar-close-button').forEach((elem) => {
+        elem.addEventListener('click', (event) => {
+          container.classList.toggle('opened');
+          container.classList.toggle('closed');
+        });
+      });
+       
+    }
+  };
